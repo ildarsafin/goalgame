fb3dc93dd8c5c21a61e41f45c99092203a2fbfb8
Ildar Safin <safild94@gmail.com>
2017-02-17 23:09:10 +0300

    change id type

--- db/migrate/20170217194402_add_tinkoff_rebill_id_to_account.rb
+++ db/migrate/20170217194402_add_tinkoff_rebill_id_to_account.rb
@@ -1,6 +1,6 @@
 class AddTinkoffRebillIdToAccount < ActiveRecord::Migration[5.0]
   def change
-    add_column :accounts, :tinkoff_rebill_id, :string
+    add_column :accounts, :tinkoff_rebill_id, :integer
 
     add_index(:accounts, :tinkoff_rebill_id)
   end
