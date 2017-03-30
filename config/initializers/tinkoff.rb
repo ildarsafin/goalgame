1c8b44eb26b897fbb7f07a8dcfd92ce02fe6ba79
Ildar Safin <safild94@gmail.com>
2017-03-30 09:44:58 +0300

    Hide Tinkoff credentials

--- config/initializers/tinkoff.rb
+++ config/initializers/tinkoff.rb
@@ -1,4 +1,4 @@
 Tinkoff.configure do |config|
-  config.terminal_key = '1487355956237DEMO'
-  config.password = 'my5nc3pgb08r9ftn'
+  config.terminal_key = ENV["TINKOFF_TERMINAL_KEY"]
+  config.password = ENV["TINKOFF_PASSWORD"]
 end
